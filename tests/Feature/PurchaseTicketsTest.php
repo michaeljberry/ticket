795bce53960d0b022a4c9c81eb301a84f9e477d1
Michael Berry <berryintlonline@gmail.com>
2018-05-25 16:00:37 -0600

    WIP

---
+++ tests/Feature/PurchaseTicketsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Concert;
+use Tests\TestCase;
+use App\Billing\FakePaymentGateway;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class PurchaseTicketsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_customer_can_purchase_concert_tickets()
+    {
+
+        $paymentGateway = new FakePaymentGateway;
+
+        $concert = factory(Concert::class)->create([
+            'ticket_price' => 3250
+        ]);
+
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $paymentGateway->getValidTestToken()
+        ]);
+
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
+
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
+
+        $this->assertEquals(3, $order->tickets->count());
+
+    }
+}
